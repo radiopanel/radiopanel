b97ac143f62a21a7b02446586819f1c94cbaff70
Pieterjan Van Saet <pieterjan.vansaet@studiohyperdrive.be>
2021-06-12 18:16:44 +0200

    feat: convert to tabs

--- app/src/lib/ui/forms/entity-type-form/entity-type-form.component.ts
+++ app/src/lib/ui/forms/entity-type-form/entity-type-form.component.ts
@@ -10,181 +10,181 @@ import camelCase from 'camelcase';
 import { DragulaService } from 'ng2-dragula';
 
 @Component({
-    selector: 'app-entity-type-form',
-    templateUrl: 'entity-type-form.component.html',
+	selector: 'app-entity-type-form',
+	templateUrl: 'entity-type-form.component.html',
 })
 export class EntityTypeFormComponent implements OnInit, OnChanges, OnDestroy {
-    @Input() entityType: any;
-    @Input() contentFields: any[];
-    @Input() workflows: any[];
+	@Input() entityType: any;
+	@Input() contentFields: any[];
+	@Input() workflows: any[];
 
-    @Output() formChange: EventEmitter<any> = new EventEmitter<any>();
+	@Output() formChange: EventEmitter<any> = new EventEmitter<any>();
 
-    private componentDestroyed$: Subject<boolean> = new Subject<boolean>();
+	private componentDestroyed$: Subject<boolean> = new Subject<boolean>();
 
-    public form: FormGroup;
-    public openFields = {};
+	public form: FormGroup;
+	public openFields = {};
 
-    constructor(
-        private dialog: MatDialog,
-        private formBuilder: FormBuilder,
-        private dragulaService: DragulaService,
-    ) {
-        this.form = this.formBuilder.group({
-            name: ['', Validators.required],
-            description: ['', Validators.required],
-            icon: ['', Validators.required],
-            slug: ['', Validators.required],
-            workflow: ['', Validators.required],
-            fields: this.formBuilder.array([])
-        });
-    }
+	constructor(
+		private dialog: MatDialog,
+		private formBuilder: FormBuilder,
+		private dragulaService: DragulaService,
+	) {
+		this.form = this.formBuilder.group({
+			name: ['', Validators.required],
+			description: ['', Validators.required],
+			icon: ['', Validators.required],
+			slug: ['', Validators.required],
+			workflow: ['', Validators.required],
+			fields: this.formBuilder.array([])
+		});
+	}
 
-    public ngOnInit(): void {
-        this.dragulaService.createGroup('fields', {
-            moves: (el, container, handle) => {
-                return el.querySelector(':scope > * > div > div > div > .m-field__handle-bar') === handle;
-            }
-        });
+	public ngOnInit(): void {
+		this.dragulaService.createGroup('fields', {
+			moves: (el, container, handle) => {
+				return el.querySelector(':scope > * > div > div > div > .m-field__handle-bar') === handle;
+			}
+		});
 
-        this.form.valueChanges
-            .pipe(
-                takeUntil(this.componentDestroyed$)
-            )
-            .subscribe((values) => this.formChange.emit(values));
-    }
+		this.form.valueChanges
+			.pipe(
+				takeUntil(this.componentDestroyed$)
+			)
+			.subscribe((values) => this.formChange.emit(values));
+	}
 
-    public ngOnChanges(changes: any) {
-        if (!changes.entityType) {
-            return;
-        }
+	public ngOnChanges(changes: any) {
+		if (!changes.entityType) {
+			return;
+		}
 
-        const entityType = changes.entityType.currentValue;
-        this.form.patchValue({
-            ...entityType,
-            workflow: pathOr('', ['workflow', 'uuid'])(entityType)
-        });
+		const entityType = changes.entityType.currentValue;
+		this.form.patchValue({
+			...entityType,
+			workflow: pathOr('', ['workflow', 'uuid'])(entityType)
+		});
 
-        const fieldsGroup = this.form.get('fields') as FormArray;
-        this.buildFieldsArray((propOr([], 'fields')(entityType) as any[])).map((formGroup) => {
-            fieldsGroup.push(formGroup);
-        });
-    }
+		const fieldsGroup = this.form.get('fields') as FormArray;
+		this.buildFieldsArray((propOr([], 'fields')(entityType) as any[])).map((formGroup) => {
+			fieldsGroup.push(formGroup);
+		});
+	}
 
-    public findFieldConfiguration(identifier) {
-        return this.contentFields.find((x) => x.identifier === identifier);
-    }
+	public findFieldConfiguration(identifier) {
+		return this.contentFields.find((x) => x.identifier === identifier);
+	}
 
-    public handleFieldsUpdate(newFieldsOrder: any[], subfieldIndex?: number) {
-        if (subfieldIndex !== undefined) {
-            return this.rebuildFieldsArray((this.form.get('fields') as FormArray).at(0).get('subfields') as FormArray, newFieldsOrder);
-        }
+	public handleFieldsUpdate(newFieldsOrder: any[], subfieldIndex?: number) {
+		if (subfieldIndex !== undefined) {
+			return this.rebuildFieldsArray((this.form.get('fields') as FormArray).at(0).get('subfields') as FormArray, newFieldsOrder);
+		}
 
-        this.rebuildFieldsArray(this.form.get('fields') as FormArray, newFieldsOrder);
-    }
+		this.rebuildFieldsArray(this.form.get('fields') as FormArray, newFieldsOrder);
+	}
 
-    public rebuildFieldsArray(formArray: FormArray, newStructure: any[]) {
-        formArray.clear();
+	public rebuildFieldsArray(formArray: FormArray, newStructure: any[]) {
+		formArray.clear();
 
-        const newFormArray = this.buildFieldsArray(newStructure);
-        newFormArray.map((formGroup) => {
-            formArray.push(formGroup);
-        });
-    }
+		const newFormArray = this.buildFieldsArray(newStructure);
+		newFormArray.map((formGroup) => {
+			formArray.push(formGroup);
+		});
+	}
 
-    private buildFieldsArray(fields) {
-        return fields.map((field) => {
-            const fieldConfiguration = this.findFieldConfiguration(field.fieldType);
+	private buildFieldsArray(fields) {
+		return fields.map((field) => {
+			const fieldConfiguration = this.findFieldConfiguration(field.fieldType);
 
-            const config = fieldConfiguration.config.reduce((acc, fieldConfig) => ({
-                ...acc,
-                [fieldConfig.identifier]: [pathOr(null, ['config', fieldConfig.identifier])(field)]
-            }), {});
+			const config = fieldConfiguration.config.reduce((acc, fieldConfig) => ({
+				...acc,
+				[fieldConfig.identifier]: [pathOr(null, ['config', fieldConfig.identifier])(field)]
+			}), {});
 
-            const fieldForm = this.formBuilder.group({
-                uuid: [field.uuid],
-                name: [field.name],
-                slug: [field.slug],
-                showOnOverview: [field.showOnOverview],
-                multiLanguage: [field.multiLanguage],
-                fieldType: [field.fieldType],
-                config: this.formBuilder.group(config),
-                subfields: this.formBuilder.array(this.buildFieldsArray(field.subfields || []))
-            });
+			const fieldForm = this.formBuilder.group({
+				uuid: [field.uuid],
+				name: [field.name],
+				slug: [field.slug],
+				showOnOverview: [field.showOnOverview],
+				multiLanguage: [field.multiLanguage],
+				fieldType: [field.fieldType],
+				config: this.formBuilder.group(config),
+				subfields: this.formBuilder.array(this.buildFieldsArray(field.subfields || []))
+			});
 
-            fieldForm.get('name').valueChanges
-                .pipe(
-                    takeUntil(this.componentDestroyed$)
-                ).subscribe((value) => fieldForm.patchValue({
-                    slug: camelCase(value)
-                }));
+			fieldForm.get('name').valueChanges
+				.pipe(
+					takeUntil(this.componentDestroyed$)
+				).subscribe((value) => fieldForm.patchValue({
+					slug: camelCase(value)
+				}));
 
-            return fieldForm;
-        });
-    }
+			return fieldForm;
+		});
+	}
 
-    public removeField(i: number, subfieldIndex?: number) {
-        const fieldsGroup = this.form.get('fields') as FormArray;
+	public removeField(i: number, subfieldIndex?: number) {
+		const fieldsGroup = this.form.get('fields') as FormArray;
 
-        if (subfieldIndex !== undefined) {
-            return (fieldsGroup.at(subfieldIndex).get('subfields') as FormArray).removeAt(i);
-        }
+		if (subfieldIndex !== undefined) {
+			return (fieldsGroup.at(subfieldIndex).get('subfields') as FormArray).removeAt(i);
+		}
 
-        fieldsGroup.removeAt(i);
-    }
+		fieldsGroup.removeAt(i);
+	}
 
-    public toggleFieldExpansion(i: number) {
-        this.openFields[i] = !pathOr(false, [i])(this.openFields);
-    }
+	public toggleFieldExpansion(i: number) {
+		this.openFields[i] = !pathOr(false, [i])(this.openFields);
+	}
 
-    public selectContentField(): void {
-        const dialogRef = this.dialog.open(ContentFieldSelectorModalComponent);
+	public selectContentField(): void {
+		const dialogRef = this.dialog.open(ContentFieldSelectorModalComponent);
 
-        dialogRef.afterClosed()
-            .pipe(
-                takeUntil(this.componentDestroyed$)
-            )
-            .subscribe((value) => {
-                if (!value) {
-                    return;
-                }
+		dialogRef.afterClosed()
+			.pipe(
+				takeUntil(this.componentDestroyed$)
+			)
+			.subscribe((value) => {
+				if (!value) {
+					return;
+				}
 
-                this.pushFieldToArray(value);
-            });
-    }
+				this.pushFieldToArray(value);
+			});
+	}
 
-    public pushFieldToArray(field, subfieldIndex?: number): void {
-        const config = (field.config || []).reduce((acc, fieldConfig) => ({
-            ...acc,
-            [fieldConfig.identifier]: ['']
-        }), {});
+	public pushFieldToArray(field, subfieldIndex?: number): void {
+		const config = (field.config || []).reduce((acc, fieldConfig) => ({
+			...acc,
+			[fieldConfig.identifier]: ['']
+		}), {});
 
-        const fieldForm = this.formBuilder.group({
-            name: [field.name],
-            slug: [''],
-            fieldType: [field.identifier],
-            multiLanguage: [false],
-            showOnOverview: [false],
-            config: this.formBuilder.group(config),
-            subfields: this.formBuilder.array([])
-        });
+		const fieldForm = this.formBuilder.group({
+			name: [field.name],
+			slug: [''],
+			fieldType: [field.identifier],
+			multiLanguage: [false],
+			showOnOverview: [false],
+			config: this.formBuilder.group(config),
+			subfields: this.formBuilder.array([])
+		});
 
-        fieldForm.get('name').valueChanges
-            .pipe(
-                takeUntil(this.componentDestroyed$)
-            ).subscribe((value) => fieldForm.patchValue({
-                slug: camelCase(value)
-            }));
+		fieldForm.get('name').valueChanges
+			.pipe(
+				takeUntil(this.componentDestroyed$)
+			).subscribe((value) => fieldForm.patchValue({
+				slug: camelCase(value)
+			}));
 
-        if (subfieldIndex !== undefined) {
-            return ((this.form.get('fields') as FormArray).at(subfieldIndex).get('subfields') as FormArray).push(fieldForm);
-        }
+		if (subfieldIndex !== undefined) {
+			return ((this.form.get('fields') as FormArray).at(subfieldIndex).get('subfields') as FormArray).push(fieldForm);
+		}
 
-        (this.form.get('fields') as FormArray).push(fieldForm);
-    }
+		(this.form.get('fields') as FormArray).push(fieldForm);
+	}
 
-    public ngOnDestroy(): void {
-        this.componentDestroyed$.next(true);
-        this.componentDestroyed$.complete();
-    }
+	public ngOnDestroy(): void {
+		this.componentDestroyed$.next(true);
+		this.componentDestroyed$.complete();
+	}
 }
